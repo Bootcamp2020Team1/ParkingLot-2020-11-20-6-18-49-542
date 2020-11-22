3cacfa32a5b415f653a1cd4d6d4641cad4dcbc6c
Jialong Zhang <JZhang182@slb.com>
2020-11-22 22:10:25 +0800

    feat: add manager

---
+++ ParkingLot/DataModels/Manager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot.DataModels
+{
+    public class Manager : ParkingBoy
+    {
+        private readonly List<ParkingBoy> managementList = new List<ParkingBoy>();
+        public Manager(Lot[] lots, List<ParkingBoy> managementList) : base(lots)
+        {
+            this.managementList = managementList;
+        }
+
+        public void AddParkingBoy(ParkingBoy parkingBoy)
+        {
+            managementList.Add(parkingBoy);
+        }
+
+        public ParkingBoy AppointBoy()
+        {
+            var appointedBoy = managementList[0];
+            return appointedBoy;
+        }
+    }
+}
