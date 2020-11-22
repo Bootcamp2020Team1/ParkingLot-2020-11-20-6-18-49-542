a1883eb7ba31acd8d36bb1bff5fcfb073ba6af6a
Xiaoyu Lu <xlu14@slb.com>
2020-11-22 12:40:00 +0800

    feat: add ParkingManager

---
+++ ParkingLot/ParkingManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ParkingLot
+{
+    public class ParkingManager : ParkingBoy
+    {
+        private List<ParkingLot> parkingLots = new List<ParkingLot>();
+        private List<ParkingBoy> parkingBoys = new List<ParkingBoy>();
+
+        public ParkingManager(List<ParkingLot> parkingLots) : base(parkingLots)
+        {
+            this.parkingLots = parkingLots;
+            this.parkingBoys.Add(this);
+        }
+
+        public void AddParkingBoy(ParkingBoy parkingBoy)
+        {
+            parkingBoys.Add(parkingBoy);
+        }
+
+        public Ticket ParkACarAndGetTicket(string license)
+        {
+            return SelectedParkingBoy().ParkACarAndGetTicket(license);
+        }
+
+        private ParkingBoy SelectedParkingBoy()
+        {
+            Random seed = new Random();
+            return parkingBoys[seed.Next(0, parkingBoys.Count)];
+        }
+    }
+}
