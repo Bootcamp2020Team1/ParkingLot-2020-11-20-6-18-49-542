e2aac0f8460d3b7e43f96ec2f6c72ada9619a3ee
Jialong Zhang <JZhang182@slb.com>
2020-11-22 19:28:05 +0800

    refactor: relocate files

---
+++ ParkingLotTest/Utils/DataLoader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ParkingLot.DataModels;
+
+namespace ParkingLotTest.Utils
+{
+    public static class DataLoader
+    {
+        public static Lot[] GetParkingLots()
+        {
+            return new Lot[]
+            {
+                new Lot("MyLot1", 1),
+                new Lot("MyLot2", 2),
+                new Lot("MyLot3"),
+            };
+        }
+    }
+}
